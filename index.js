3755b67f07a981d0d6179fcd166e84624ff6f632
mildzik <miloszdzikowski01@gmail.com>
2024-02-22 10:51:55 +0000

    accurate ratings
    Each animal entry now has its own endangerment rating instead of the default

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var speciesList = ["Sea Turtle", "Penguin", "Dolphin", "Seal","Blob"];
+    var speciesList = ["Hawksbill Turtle", "Vaquita", "Leatherback Turtle", "Blue Whale","Stellar Sea Lion","Monk Seal","Green Sea Turtle","Nile Crocodile","Ganges River Dolphin"];
+    var imgList = ["hawksbill_turtle.jpg","vaquita.jpg","leatherback_turtle.jpg","blue_whale.jpg","stellar_sea_lion.jpg","monk_seal.jpg","green_sea_turtle.jpg","nile_crocodile.jpg","ganges_river_dolphin.jpg"]
+    var levelList = [5,5,2,3,3,3,3,0,3]
+    var endangerList = ["least_concern.png", "near_threatened.png", "vulnerable.png", "endangered.png","critically_endangered.png","extinct_in_the_wild.png","extinct.png"]
     var speciesListContainer = document.getElementById("speciesListContainer");
 
     for (var i = 0; i < speciesList.length; i++) {
@@ -10,17 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
         speciesLink.href = "species-page.html"; //Find a way for the site to find source so that it dynamically changes
 
         var speciesImage = document.createElement("img");
-        speciesImage.alt = "image";
-        speciesImage.src = "https://play.teleporthq.io/static/svg/default-img.svg"; //Change Image based on the animal
+        speciesImage.alt = "animal-image";
+        speciesImage.src = "images/" + imgList[i];
         speciesImage.className = "species-image";
+        
         var speciesName = document.createElement("span");
         speciesName.className = "species-name";
         speciesName.innerHTML = "<span>" + speciesList[i] + "</span>";
 
-        //add the rating system
+        var ratingImage = document.createElement("img")
+        ratingImage.alt = "rating-image"
+        ratingImage.src = "images/" + endangerList[levelList[i]]
+        ratingImage.width = 145
+        ratingImage.className = "species-rating"
 
         speciesLink.appendChild(speciesImage);
         speciesLink.appendChild(speciesName);
+        speciesLink.appendChild(ratingImage);
 
         speciesContainer.appendChild(speciesLink);
 

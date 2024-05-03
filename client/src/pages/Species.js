09af33db67e01489699bea8229f4970011698f8f
Kieran White <124150925+Kieran-White@users.noreply.github.com>
2024-05-03 11:14:19 +0100

    Updated the Layout for the Species Page

--- client/src/pages/Species.js
+++ client/src/pages/Species.js
@@ -1,9 +1,27 @@
+import "./Species.css";
+
 export default function Species(){
     return (
         <>
-            <h1>h1</h1>
-            <p>p1</p>
-            <p>p2</p>
+            
+            <div className="SpeciesInfoGrid">
+                <img
+                    src="{}"
+                    className="SpeciesImage"
+                />
+                <h2>Status: {}</h2>
+                <p>Common Name: {}</p>
+                <p>Scientific Name: {}</p>
+                <p>Length: {}</p>
+                <p>Habitats: {}</p>
+                <img
+                    src="{}"
+                    className="StatusImage"
+                />
+            </div>
+            <div className="SpeciesText">
+                <p>Species text information</p>
+            </div>
         </>
     );
 }

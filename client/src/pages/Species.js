819c146bc41559a86778c1b05d6c30a0f71c8faf
Kieran White <124150925+Kieran-White@users.noreply.github.com>
2024-05-03 12:13:16 +0100

    Started to add functionality to the Species page

--- client/src/pages/Species.js
+++ client/src/pages/Species.js
@@ -1,19 +1,27 @@
 import "./Species.css";
 
 export default function Species(){
+
+    const speciesImg = "";
+    const speciesStatus = "";
+    const speciesCommon = "";
+    const specScientific = "";
+    const specLength = "";
+    const specHabitats = "";
+
     return (
         <>
             
             <div className="SpeciesInfoGrid">
                 <img
-                    src="{}"
+                    src={speciesImg}
                     className="SpeciesImage"
                 />
-                <h2>Status: {}</h2>
-                <p>Common Name: {}</p>
-                <p>Scientific Name: {}</p>
-                <p>Length: {}</p>
-                <p>Habitats: {}</p>
+                <h2>Status: {speciesStatus}</h2>
+                <p>Common Name: {speciesCommon}</p>
+                <p>Scientific Name: {specScientific}</p>
+                <p>Length: {specLength}</p>
+                <p>Habitats: {specHabitats}</p>
                 <img
                     src="{}"
                     className="StatusImage"

d706267c28a5146e8b6ddf05fdcc4ce7be4ca61b
bigboiearnes <122842632+bigboiearnes@users.noreply.github.com>
2024-05-03 13:15:55 +0100

    Added API route to get all species
    API route at '/api/species' will now return every species currently in the database

--- client/src/components/FetchSpeciesData.js
+++ client/src/components/FetchSpeciesData.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-
+// Pass this function a species name and it will return all of its data.
 const useFetchSpeciesData = (speciesName) => {
     const [species, setSpecies] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -13,6 +13,7 @@ const useFetchSpeciesData = (speciesName) => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch data')
                 }
+                console.log(response)
                 const data = await response.json();
                 setSpecies(data);
                 setLoading(false);

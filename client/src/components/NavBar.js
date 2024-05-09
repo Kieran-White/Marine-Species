9cd00404998279ed824e346e89842c6db6027eab
Kieran White <124150925+Kieran-White@users.noreply.github.com>
2024-05-09 10:15:37 +0100

    Rearranged the nav bar to fit the interface desisn

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,18 +5,20 @@ export default function NavBar(){
     return (
         <div class='nav-bar-container'>
             <h1 class='home-title'>Endangered Species</h1>
-            <nav class='nav-bar-navs'>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/oceanmap">OceanMap</NavLink>
-                <NavLink to="/donations">Donations</NavLink>                
-                <NavLink to="/articles">Articles</NavLink>
-                <NavLink to="/species">Species</NavLink>   
-            </nav>
-            <input 
-                type="text" 
-                placeholder="Search" 
-                class='nav-bar-search-bar'
-            />
+            <div class="navSearch">
+                <nav class='nav-bar-navs'>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/oceanmap">OceanMap</NavLink>
+                    <NavLink to="/donations">Donations</NavLink>                
+                    <NavLink to="/articles">Articles</NavLink>
+                    <NavLink to="/species">Species</NavLink>   
+                </nav>
+                <input 
+                    type="text" 
+                    placeholder="Search" 
+                    class='nav-bar-search-bar'
+                />
+            </div>
         </div>
     );
 }

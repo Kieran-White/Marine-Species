1d30670c8508fcae324281a6822bba9eed6dae54
Kieran White <124150925+Kieran-White@users.noreply.github.com>
2024-05-15 11:11:52 +0100

    Added more Species

--- client/src/components/SpeciesList.js
+++ client/src/components/SpeciesList.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 import "./SpeciesList.css";
 
 export default function SpeciesList() {
-    const speciesList = ["Hawksbill Turtle", "Vaquita", "Leatherback Turtle", "Blue Whale", "Sea Lions", "Green Turtle", "Ganges River Dolphin"];
-    const imgList = ["hawksbill_turtle.jpg", "vaquita.jpg", "leatherback_turtle.jpg", "blue_whale.jpg", "sea_lions.jpg", "green_turtle.jpg", "ganges_river_dolphin.jpg"]
-    const levelList = [4, 4, 2, 3, 3, 3, 1, 3]
+    const speciesList = ["Hawksbill Turtle", "Vaquita", "Leatherback Turtle", "Blue Whale", "Sea Lions", "Green Turtle", "Ganges River Dolphin", "Yellowfin Tuna", "Narwhal"];
+    const imgList = ["hawksbill_turtle.jpg", "vaquita.jpg", "leatherback_turtle.jpg", "blue_whale.jpg", "sea_lions.jpg", "green_turtle.jpg", "ganges_river_dolphin.jpg", "yellowfin_tuna.jpg", "narwhal.jpg"]
+    const levelList = [4, 4, 2, 3, 3, 3, 3, 1, 0]
     const endangerList = ["least_concern.png", "near_threatened.png", "vulnerable.png", "endangered.png", "critically_endangered.png", "extinct_in_the_wild.png", "extinct.png"];
 
     return (

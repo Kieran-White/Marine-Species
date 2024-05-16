66af9db348f7f3a05dae946347d791c8ecd05877
Kieran White <124150925+Kieran-White@users.noreply.github.com>
2024-05-16 12:59:39 +0100

    Made Homepage dynamically show species from db

--- client/src/components/SpeciesList.js
+++ client/src/components/SpeciesList.js
@@ -1,20 +1,47 @@
 import { Link } from 'react-router-dom';
 import "./SpeciesList.css";
+import useFetchSpeciesData from './FetchSpeciesData';
+import { useEffect } from 'react';
 
 export default function SpeciesList() {
-    const speciesList = ["Hawksbill Turtle", "Vaquita", "Leatherback Turtle", "Blue Whale", "Sea Lions", "Green Turtle", "Ganges River Dolphin", "Yellowfin Tuna", "Narwhal"];
-    const imgList = ["hawksbill_turtle.jpg", "vaquita.jpg", "leatherback_turtle.jpg", "blue_whale.jpg", "sea_lions.jpg", "green_turtle.jpg", "ganges_river_dolphin.jpg", "yellowfin_tuna.jpg", "narwhal.jpg"]
-    const levelList = [4, 4, 2, 3, 3, 3, 3, 1, 0]
+
+    const { data, loading, error } = useFetchSpeciesData();
+
+    useEffect(() => {
+
+    });
+
+    console.log(data);
+    
+    if (loading) {
+    return <div>Loading...</div>;
+    }
+
+    if (error) {
+    return <div className='profile-page-wrapper'>Error: {error.message}</div>;
+    }
+
+    if (!data || data.length === 0) {
+        return <div className='profile-page-wrapper'>No Data Found</div>
+    }
+
     const endangerList = ["least_concern.png", "near_threatened.png", "vulnerable.png", "endangered.png", "critically_endangered.png", "extinct_in_the_wild.png", "extinct.png"];
 
     return (
         <div className="home-species-list" id="speciesListContainer">
-            {speciesList.map((species, index) => (
+            {data.map((speciesData, index) => (
                 <div key={index} className="species-container species-root-class-name">
-                    <Link to={`/species/${encodeURIComponent(species)}`}>
-                        <img alt="animal-image" src={require(`../images/${imgList[index]}`)} className="species-image" />
-                        <span className="species-name">{species}</span>
-                        <img alt="rating-image" src={require(`../images/${endangerList[levelList[index]]}`)} width="145" className="species-rating" />
+                    <Link to={`/species/${encodeURIComponent(speciesData.species)}`}>
+                        <img
+                            alt={`${speciesData.species}-image`} 
+                            src={require(`../images/${speciesData.speciesImg}`)}
+                            className="species-image" />
+                        <span className="species-name">{speciesData.species}</span>
+                        <img
+                        alt="rating-image"
+                        src={require(`../images/${endangerList[speciesData.endangerLevel]}`)}
+                        width="145"
+                        className="species-rating" />
                     </Link>
                 </div>
             ))}
